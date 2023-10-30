353efb7172ea25a525881a015d671f12fd1e676c
KyungsooLee <jmtkd9196@gmail.com>
2023-10-30 18:38:14 +0900

    feat: Init Project

---
+++ BackDaBang/src/main/java/umc/BackDaBang/BackDaBangApplication.java
@@ -0,0 +1,13 @@
+package umc.BackDaBang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackDaBangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackDaBangApplication.class, args);
+	}
+
+}
---
+++ BackDaBang/src/test/java/umc/BackDaBang/BackDaBangApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.BackDaBang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackDaBangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
