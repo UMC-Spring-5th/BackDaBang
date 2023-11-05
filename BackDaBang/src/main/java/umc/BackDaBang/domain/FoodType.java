6269f3fda08364c7ef40985a7f5a557085681c45
KyungsooLee <jmtkd9196@gmail.com>
2023-11-06 00:32:19 +0900

    chore: FoodType entity setting

---
+++ BackDaBang/src/main/java/umc/BackDaBang/domain/FoodType.java
@@ -0,0 +1,29 @@
+package umc.BackDaBang.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class FoodType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = true, length = 20)
+    private String detail;
+}
