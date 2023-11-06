7497cfa4bc913b48d7f3f5c19c0322897c54935c
KyungsooLee <jmtkd9196@gmail.com>
2023-11-06 17:36:36 +0900

    chore: SocialType Enum setting

---
+++ BackDaBang/src/main/java/umc/BackDaBang/domain/SocialType.java
@@ -0,0 +1,14 @@
+package umc.BackDaBang.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SocialType {
+
+    KAKAO,
+    GOOGLE,
+    APPLE;
+
+}
