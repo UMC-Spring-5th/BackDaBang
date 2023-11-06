371a84dbe2ac5e0ed73e61ab1ed312091f41ba67
KyungsooLee <jmtkd9196@gmail.com>
2023-11-06 17:37:34 +0900

    chore: Gender Enum setting

---
+++ BackDaBang/src/main/java/umc/BackDaBang/domain/Gender.java
@@ -0,0 +1,13 @@
+package umc.BackDaBang.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Gender {
+
+    MALE,
+    FEMALE;
+
+}
