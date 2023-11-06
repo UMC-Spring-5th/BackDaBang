cf5e43487b1b9b6fab616f4601609e3ed0525ac3
KyungsooLee <jmtkd9196@gmail.com>
2023-11-06 17:14:23 +0900

    chore: Member entity setting

--- BackDaBang/src/main/java/umc/BackDaBang/domain/Member.java
+++ BackDaBang/src/main/java/umc/BackDaBang/domain/Member.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -25,21 +25,21 @@ public class Member {
     private String email;
 
     @Column(nullable = false, length = 10)
-    private String socialType;
+    private Enum socialType;
 
     @Column(nullable = false, length = 20)
     private String name;
 
     @Column(nullable = true, length = 10)
-    private String gender;
+    private Enum gender;
 
     @Column(nullable = false)
-    private LocalDateTime birthday;
+    private LocalDate birthday;
 
     @Column(nullable = false, length = 255)
     private String address;
 
-    @Column(nullable = true, length = 10)
+    @Column(nullable = true, length = 13)
     private String phoneNumber;
 
     @Column(nullable = true)
