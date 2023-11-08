7a67f90df865808953c071642de954d6ccb1811b
tnals2384 <tnals2384@gmail.com>
2023-11-08 16:24:27 +0900

    API 응답 통일

---
+++ BackDaBang/src/main/java/umc/BackDaBang/apiPayload/code/BaseCode.java
@@ -0,0 +1,9 @@
+package umc.BackDaBang.apiPayload.code;
+
+
+//구체화 하는 Status에서 두개의 메소드를 반드시 Override할 것을 강제
+public interface BaseCode {
+    public ReasonDTO getReason();
+
+    public ReasonDTO getReasonHttpStatus();
+}
