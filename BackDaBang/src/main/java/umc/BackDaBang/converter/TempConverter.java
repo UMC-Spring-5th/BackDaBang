3c2589a611ba3e6bc476228586e3ed23960b7562
KyungsooLee <jmtkd9196@gmail.com>
2023-11-29 22:50:57 +0900

    feat: Converter 구현

---
+++ BackDaBang/src/main/java/umc/BackDaBang/converter/TempConverter.java
@@ -0,0 +1,12 @@
+package umc.BackDaBang.converter;
+
+import umc.BackDaBang.web.dto.TempResponse;
+
+public class TempConverter {
+
+    public static TempResponse.TempTestDTO toTempTestDTO() {
+        return TempResponse.TempTestDTO.builder()
+                .testString("This is Test!")
+                .build();
+    }
+}
